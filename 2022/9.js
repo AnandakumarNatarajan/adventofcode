a8fd0d68cb68853373d037cbae89b50578be4ca7
anand-3985 <anandakumar.n@zohocorp.com>
2022-12-12 11:13:29 +0530

    Day 9

--- 2022/9.js
+++ 2022/9.js
@@ -1,68 +1,76 @@
 import { readLines } from "./fileutils.mjs";
 
 
-const bridge = function() {
+const bridgewithnknots = function( num ) {
 
     let map = new Map()
-    let count = 0
-    let i=0,j=0
-    let ti=0, tj=0
+    let count=0
+    
+    let arrti = new Array(num)
+    let arrtj = new Array(num)
 
-    function moveable() {
-        return Math.abs( i-ti ) > 1 || Math.abs( j-tj ) > 1
+    arrti.fill(0)
+    arrtj.fill(0)
+
+    function moveable( a,b,x,y ) {
+        return Math.abs( a-x ) > 1 || Math.abs( b-y ) > 1
     }
 
     function movehandt( d ) {
         switch( d ) {
             case 'U' :
-                i++
+                arrti[0]++
                 break
             case 'D' :
-                i--
+                arrti[0]--
                 break
             case 'R' :
-                j++
+                arrtj[0]++
                 break
             case 'L' :
-                j--
+                arrtj[0]--
                 break
         }
-        console.log(moveable())
-        if( moveable() ) {
-            if( i == ti ) {
-                if( j > tj ) {
-                    tj++
+        let n = 1;
+        
+        while( num > n ) {
+            //console.log(moveable( arrti[n],arrtj[n], arrti[n-1],arrtj[n-1] ))
+     
+            if( moveable( arrti[n],arrtj[n], arrti[n-1],arrtj[n-1] ) ) {
+                if( arrti[n-1] == arrti[n] ) {
+                    if( arrtj[n-1] > arrtj[n] ) {
+                        arrtj[n]++
+                    } else {
+                        arrtj[n]--
+                    }
+                } else if ( arrtj[n-1] == arrtj[n] ) { 
+                    if( arrti[n-1] > arrti[n] )
+                    arrti[n]++
+                    else
+                    arrti[n]--
                 } else {
-                    tj--
+                    if( arrti[n-1] > arrti[n] && arrtj[n-1] > arrtj[n] ) {
+                        arrti[n]++
+                        arrtj[n]++
+                    } else if ( arrti[n-1] > arrti[n] && arrtj[n-1] < arrtj[n] ) {
+                        arrti[n]++
+                        arrtj[n]--
+                    } else if ( arrti[n-1] < arrti[n] && arrtj[n-1] < arrtj[n] ) {
+                        arrti[n]--
+                        arrtj[n]--
+                    } else {
+                        arrti[n]--
+                        arrtj[n]++
+                    }
                 }
-            } else if ( j == tj ) {
-                if( i > ti )
-                    ti++
-                else
-                    ti--
-            } else {
-                if( i > ti && j > tj ) {
-                    ti++
-                    tj++
-                } else if ( i > ti && j < tj ) {
-                    ti++
-                    tj--
-                } else if ( i < ti && j < tj ) {
-                    ti--
-                    tj--
-                } else {
-                    ti--
-                    tj++
-                }
+
+                if( n == num -1 && !map.has(`${arrti[n]}:${arrtj[n]}`) ){
+                    count++
+                    map.set(`${arrti[n]}:${arrtj[n]}`,true)
+                }    
             }
-
-            if( !map.has(`${ti}:${tj}`) )
-                count++
-            else    
-                map.set(`${ti}:${tj}`,true)
-
+            n++
         }
-        console.log(`${i} ${j} : ${ti} ${tj}` )
 
     } 
 
@@ -78,11 +86,9 @@ const bridge = function() {
 }
 
 
-async function part() {
-
+async function part1() {
     const lines = await readLines('./input/9.txt')
-
-    const b = bridge()
+    const b = bridgewithnknots(1+1 /*1 head + 1 tail*/)
 
     for await ( let line of lines ) {
         //console.log( line )
@@ -94,119 +100,27 @@ async function part() {
             s--
         }
     }
-
-    console.log(b.get())
-
+    console.log(b.get() + 1 )
 }
 
-await part();
+async function part2() {
+    const lines = await readLines('./input/9.txt')
+    const b = bridgewithnknots(1+9 /*1head 9 tails*/)
 
-// const ropebride = function() {
-  
-//     let hi = 0
-//     let hj = 0
-//     let ti = 0
-//     let tj = 0
-//     let map = new Map() 
-//     let prev = null
-//     let count =0;
+    for await ( let line of lines ) {
+        //console.log( line )
+        let m = line.split(' ')
+        let s = parseInt( m[1] )
+        
+        while(s) {
+            b.move(m[0])
+            s--
+        }
+    }
+    console.log(b.get() + 1 )
+}
 
-//     function setmap( key ) {
-//         if(!map.has( key )) {
-//             map.set( key, true )
-//             count++
-//         }
-//     }
 
-//     function movetail(dir, step) {
-//         switch( dir ){
-//             case 'U' : 
-//                 for( let i=ti; i < ti+step; i++ )
-//                     setmap(`${i}.${tj}`)
-//                 ti+=step
-//                 break
-//             case 'D' :
-//                 for( let i=ti; i < ti-step; i-- )
-//                     setmap(`${i}.${tj}`)
-//                 ti-=step
-//                 break
-//             case 'R' : 
-//                 for( let j=tj; j < tj+step; j++ )
-//                     setmap(`${ti}.${j}`)
-//                 tj+=step
-//                 break
-//             case 'L' : 
-//                 for( let j=tj; j < tj-step; j++ )
-//                     setmap(`${ti}.${j}`)
-//                 tj-=step
-//                 break
-//         }
-//     }
 
-//     function moveHead( dir, step ) {
-//         if( !prev ) {
-//             switch( dir ){
-//                 case 'U' : 
-//                     hi+=step
-//                     break
-//                 case 'D' :
-//                     hi-=step
-//                     break
-//                 case 'R' : 
-//                     hj-=step
-//                     break
-//                 case 'L' : 
-//                     hj+=step
-//                     break
-//             }
-//             movetail(dir,step-1)
-//             prev = dir
-//         }
-
-//         if( prev == dir ) {
-//             movetail(dir,step-1)
-//         } else if( ( prev == 'R' && dir == 'L') || ( prev == 'L' && dir == 'R' ) ) {
-//             movetail(dir,step-2)
-//         } else if( ( prev == 'U' && dir == 'D') || ( prev == 'D' && dir == 'U' ) ) {
-//             movetail(dir,step-2)       
-//         } else {
-//             switch( dir ){
-//                 case 'U' : 
-//                     hi+=step
-//                     break
-//                 case 'D' :
-//                     hi-=step
-//                     break
-//                 case 'R' : 
-//                     hj-=step
-//                     break
-//                 case 'L' : 
-//                     hj+=step
-//                     break
-//             }
-//         }
-//     }
-    
-//     return {
-//          move(dir, step){
-//             moveHead(dir,step)
-//          }
-//     }
-// }
-
-// async function part(  ) {
-//     const lines = await readLines ('./input/9.txt')
-
-//     let count = 0
-//     let prev
-
-//     const bridge = readLines()
-//     for await (let line of lines) {
-
-//         let move = line.split(' ')
-//         bridge.move(move[0], parseInt( move[1] ) )
-
-//     }
-// }
-
-// await part()
+await part1();
+await part2();
